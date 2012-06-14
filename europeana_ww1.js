f368b4411f52a9486d03ebc1007fb6c116405289
Lee Baylis <mpciiljb@Mimass-MacBook-Pro.local>
2012-06-15 00:59:34 +0200

    commented the routines

--- europeana_ww1.js
+++ europeana_ww1.js
@@ -31,6 +31,10 @@ var EWW1={
 	return html + "</ul>";
     },
 
+    category_metadata_field: function(category){
+	return this.categories[category].meta;
+    },
+
     //UI functions
     visible_stories: {},
 
@@ -87,10 +91,7 @@ var EWW1={
 	return base.enclosure || base["europeana:object"];
     },
 
-    category_metadata_field: function(category){
-	return this.categories[category].meta;
-    },
-
+    //Europeana API specific URI encoders/decoders
     decode: function(str){
 	return decodeURI(str).replace(/%3A/g, ":");
     },
@@ -101,6 +102,7 @@ var EWW1={
 	else {return "'" + rawstr + "'";}
     },
 
+    //convert URIs between return data types requested
     returnData: {
 	to: function(str, querytype){
 	    if (str.match(/\.(html|json|rss)$/i)){
@@ -114,6 +116,7 @@ var EWW1={
 	RSS: function(str){return this.to(str, "rss");}
     },
 
+    //convert URIs between image sizes
     imageSize: {
 	to: function(str, size){
 	    return str.replace(/\.[^\.]*.(jpg|bmp|pdf)$/i, "." + size + ".$1");},
@@ -123,7 +126,7 @@ var EWW1={
 	medium: function(str){return this.to(str, "medium");}
     },
 
-    //API Queries
+    //API Queries. a lot of repeat code here, could be tidied up.
 
     queries: {
 	full_record: function(record){
@@ -147,13 +150,6 @@ var EWW1={
 	    return search;
 	},
 
-	page: function(query,page){
-	    if(!query.match(/&wskey=/)) {query = query + "&" + EWW1.query_parts.key;}
-	    var start = "&startPage=" + page;
-	    if (query.match(/startPage=/)){return query.replace(/&startPage=[0-9]+/, start);}
-	    else {return query + start;}
-	},
-
 	category_entries: function(category){
 	    var stories_part = arguments[1] ? "dc_type:story+" : "";
 	    var query = EWW1.query_parts.base + 
@@ -170,9 +166,29 @@ var EWW1={
 	    "dc_relation:\"" + encodeURI(story) + "\"+" +
 	    EWW1.query_parts.ww1_collection + "&" + 
 	    EWW1.query_parts.key;
+	},
+
+	//used by aggregation routine -- get the "page"th page for some existing query
+	page: function(query,page){
+	    if(!query.match(/&wskey=/)) {query = query + "&" + EWW1.query_parts.key;}
+	    var start = "&startPage=" + page;
+	    if (query.match(/startPage=/)){return query.replace(/&startPage=[0-9]+/, start);}
+	    else {return query + start;}
 	}
     },
 
+    /* main entry point to the code. This function will run your query and pass the result data to a callback function. A context parameter which will be the value of 'this' in the callback can be supplied in the options.
+       By supplying options.limit or options.full, you can activate the built-in aggregator which will transparently override europeana's hard limit on the number of queries returned.
+
+       str: the full query string you would normally issue to the Europeana API including the API key
+       callback: the callback function you would normally pass to JSON-P
+       options: a JSON object containing the following:
+         context: the object which will become 'this' in the callback
+	 limit: the maximum number of records to return. If not set, defaults to RESULT_LIMIT
+	 full: if set to true, run full portal queries for each record returned and replace the normal short summaries with the full ones in the results
+  
+	 (aggregator: internal use only. If aggregation is already running, the aggregation routine should pass this parameter.)
+    */
     query: function(str, callback){
 	var ag;
 	var options = arguments[2] || {};
@@ -180,6 +196,7 @@ var EWW1={
 	if (!options.aggregator && 
 	    (options.full || options.limit > this.RESULT_LIMIT)
 	    ){
+	    //aggregation has been implied from options -- create an aggregator and divert callback to it
 	    options.aggregator = new EWW1.Aggregator(options.limit, callback, options);
 	    callback = options.aggregator.callback;
 	}
@@ -187,7 +204,7 @@ var EWW1={
 	var ajax_options = {dataType: "jsonp", success: callback};
 
 	if(options.aggregator){
-	    console.log("aggregating");
+	    //divert context to the aggregator as well
 	    ajax_options.context = options.aggregator;
 	}
 	else {ajax_options.context = options.context}
@@ -196,20 +213,22 @@ var EWW1={
     },
 
     //Aggregator to solve the problem of europeana only returning RESULT_LIMIT results at a time maximum
-
+    //Constructor
     Aggregator: function(number, callback){
 	options = arguments[2] || {};
 	//do several queries and store results here
 	this.results = [];
 	//optional store for full records
 	this.full_records = [];
+
 	this.limit = number;
 	this.detail = options.full;
 
-	//when finished, tell aggregator to call the callback from the original query
+	//when aggregation finished, restore the callback and context from the original query
 	this.complete = callback;
 	this.complete_context = options.context;
-
+	
+	//extend the aggregator
 	var methods = EWW1.Aggregator_methods;
 	for (var method in methods){
 	    this[method] = methods[method];
@@ -217,22 +236,31 @@ var EWW1={
     },
 
     Aggregator_methods: {
+	//this replaces the callback suppled by the user in order to carry on with the aggregation by running successive page queries.
 	callback: function(data){
+	    //data must be a container -- adjust single results
 	    data = data.items ? data : {items: [data]};
+	    //store the results of this query
 	    this.results.push(data);
+	    //internal record count
 	    this.itemsSoFar = this.results.length*data.itemsPerPage;
+	    //reduce limit to the max number of results available if it is greater than this
 	    if (data.totalResults < this.limit){this.limit = data.totalResults;}
-
+	    //get full records for this data if they have been requested
 	    if (this.detail){this.get_detail(data);}
 
+	    //are we done?
 	    if (this.itemsSoFar < this.limit && this.limit > this.itemsSoFar){
+		//no. Issue next query and repeat.
 		EWW1.query(EWW1.queries.page(EWW1.format_for_query(EWW1.decode(data.link),"strip"), this.results.length + 1), this.callback, {aggregator: this});
 	    }
 	    else {
+		//yes, unless full details have been requested. Otherwise perform the actual aggregarion and return the aggregated data to the original callback and context requested.
 		if (!this.detail){this.complete.call(this.complete_context, this.aggregate());}
 	    }
 	},
 	
+	//run additional optional queries to get full data
 	get_detail: function(data){
 	    console.log("fetch " + data.items.length + " records");
 	    for (var i=0; i < data.items.length; i++){
@@ -240,9 +268,11 @@ var EWW1={
 	    }
 	},
 
+	//process any full data queries which return
 	full_callback: function(data){
 	    this.full_records.push(data);
 
+	    //if full records have been requested, perform the actual aggregarion and return the aggregated data to the original callback and context requested.
 	    if (this.full_records.length == this.itemsSoFar && 
 		this.itemsSoFar == this.limit)
 		{
@@ -250,6 +280,7 @@ var EWW1={
 		}
 	},
 
+	//merge each page's items array into a single array. Retain the other data returned by the pages.
 	aggregate: function(){
 	    if (!this.aggregated_results){
 		this.aggregated_results = jQuery.extend({},this.results[0]);
@@ -341,6 +372,7 @@ var EWW1={
 	    EWW1.query(EWW1.queries.full_record(result_record), EWW1.callbacks.map_stories, {context: {passthrough_callback: EWW1.callbacks.render_full_story, passthrough_into: "full_story"}});
 	},
 
+	//David's original routine
 	logResults: function( data ) {
 	    if ( window.console && console.log ) {
 		if ( data ) {
